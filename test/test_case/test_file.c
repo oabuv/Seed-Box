a0a192a07b40259049c1591d89b30264b1ccb9ae
Bao Vu <oab.uv9@gmail.com>
2022-05-08 17:05:36 +0300

    test cases update

--- test/test_case/test_file.c
+++ test/test_case/test_file.c
@@ -1,13 +1,14 @@
 #include "project_functions.h"
 
-TEST(moistureReader, tooHighValue){
+// Test cases for soil sensor
+TEST(moistureReader, MaxValueExceeded){
     int ADC_val = 4095;
     double percentage = moisture_percentage(ADC_val);
 
     EXPECT_EQ(percentage, 100.00);
 }
 
-TEST(moistureReader, tooLowValue){
+TEST(moistureReader, MinValueExceeded){
     int ADC_val = 1;
     double percentage = moisture_percentage(ADC_val);
 
@@ -21,8 +22,22 @@ TEST(moistureReader, normalValue){
     EXPECT_EQ(percentage, 50);
 }
 
+// Test cases for light sensor
+TEST(luxReader, tooDark){
+    int ADC_val = 10000;
+    int lux = measured_lux(ADC_val);
 
-TEST(luxReader, normalValue){
+    EXPECT_EQ(lux, 10);
+}
+
+TEST(luxReader, tooBright){
+    int ADC_val = 100;
+    int lux = measured_lux(ADC_val);
+
+    EXPECT_EQ(lux, 5535);
+}
+
+TEST(luxReader, normalLight){
     int ADC_val = 700;
     int lux = measured_lux(ADC_val);
 
